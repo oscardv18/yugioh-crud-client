f7ba1955b86ec53f1b6b8731f8f52fc436276db8
oscardv18 <oscarddiazvelasquez@gmail.com>
2023-07-07 21:43:47 -0400

    landing page maquetado terminado

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { LoginDialogComponent } from '../login-dialog/login-dialog.component';
 
 @Component({
   selector: 'app-nav',
@@ -8,18 +6,7 @@ import { LoginDialogComponent } from '../login-dialog/login-dialog.component';
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
-  links: string[] = ['Inicio', 'Cartas', 'Registro', 'Login'];
+  links: string[] = ['Inicio', 'Cartas', 'Registro', 'Iniciar Sesion'];
 
-  constructor(public dialog: MatDialog) {}
-
-  openLoginDialog(): void {
-    const dialogRef = this.dialog.open(LoginDialogComponent, {
-      width: '300px',
-      disableClose: true,
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log('Diálogo cerrado');
-    });
-  }
+  constructor() {}
 }
